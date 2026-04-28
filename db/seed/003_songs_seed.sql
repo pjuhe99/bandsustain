526f798792c401ced518e5f18fcb5d916bcc9032
pjuhe99 <soritunenglish@gmail.com>
2026-04-28 13:28:50 +0900

    Generate seed SQL from src/data/* and apply to BANDSUSTAIN DB

---
+++ db/seed/003_songs_seed.sql
@@ -0,0 +1,138 @@
+-- 003_songs_seed.sql (auto-generated)
+
+INSERT INTO songs (title, category, artwork_url, listen_url, lyrics, released_at, published) VALUES
+('내 그림자가 멀어져 (Fading shadow)', 'Single', '/songs/song01.jpg', 'https://youtu.be/eQNgAR6R8Tc?si=dodLMXBdwL6o3JmK', '내 그림자가 멀어져 손 닿지 않게
+내가 빛을 잃어가 내게 빛을 조금만 줘
+너도 알 수 있게 아른거리는 내게
+
+색깔을 잃어버려 가는 나의 모습
+떨어지는 꽃들이 나풀거려
+어딘가 모르겠는 낯선 이 거리에
+물에 잠긴 하늘을 봐
+어둑한 골목 하나 뒤 돌아보며
+
+너의 마음을 줄래 내가 알 수 있게
+내가 너를 느끼게 네게 내 마음을 줄게
+널 느낄 수 있게 희미 해지는 내가
+
+찾아보면 멀어져 버린 너의 온기
+흩어 져버린 향기가 아른거려
+사랑했던 모든게 떠난 이 도시에
+추락한 내 모습을 봐
+홀로 뜬 달빛 한 번 올려보며
+
+내 그림자가 멀어져 손 닿지 않게
+내가 빛을 잃어가 네게 내 마음을 줄게
+널 느낄 수 있게 희미해지는 내가', '2023-05-05', 1),
+('별꿈 (Dreaming star)', 'Single', '/songs/song02.jpg', 'https://youtu.be/mQ8WDaaVl1Y', '하늘 사이로 쏱아지는 별빛을 봐
+흘러가는 시간들이 나를 비추고
+떨어지는 빛의 궤적이 널 부르면
+우리는 그곳을 향해 손을 뻗어
+
+이젠 없는거야 너와 나의 시간은
+그리울 거야 영원할 것만 같아
+이제는 모든 것이 끝나가고 있지만
+한 가지 남아 있는건 너와 나의 기억들
+
+펼쳐진 밤하늘에 쏱아지는 별빛에
+너와 내가 꿈을 그린 모든 날들
+쏱아지는 은하에 타오르는 별빛에
+너와 내가 함께했던 모든 시간들을
+그리워하며 애태우다가 이제는
+돌아갈 수 없는 것을 알아
+이제는 없어 나의 기억속으로
+살아가겠지
+
+이젠 없을거야 너와 나의 기억도
+그리웠지만 잊혀져만 가겠지
+이제는 너와 내가 멀어져 가고있는
+세상에 남아 있는건 너와 나의 사랑뿐
+
+펼쳐진 밤하늘과 멀어진 감정들에
+네가 내가 해주었던 많은 말들속에
+내가 널 기억 하고 오롯이 살아있는
+내가 너를 끝까지 기억해 줄 테니까
+그리워하며 깨우쳐간 것은 내가
+너를 좋아하는 기분인걸
+사랑했는데 이제는 없는 널
+기억하겠지', '2023-10-17', 1),
+('이세계로 초대할게 (Welcome to another world)', 'Single', '/songs/song03.jpg', 'https://youtu.be/C28QejgyOJ4', '이젠 떠날 시간이 됐어
+마음의 짐은 모두 덜어버렸니
+행복한 것들만 생각해도
+우리 인생은 너무나도 짧잖아
+
+이세계가 숨쉬는 동안
+살아움직이는 모든 것들이 전부
+니 세계를 더 아름답게해
+
+열리지 않은 길의 끝은 어딘가
+익숙치 않은 느낌이 나도 좋아
+거짓말도 재밌게 한다면 괜찮아
+어제 네가 해준 이야기가 말이야
+
+다시 들려 줄 수 있겠니
+요정과 기사와 마법들이 가득한
+니가 내게 해준 이세계 여행
+
+이세계가 숨쉬는 동안
+살아움직이는 모든 것 들이 전
+니 세계를 더 아름답게해
+다시 들려 줄 수 있겠니
+요정과 기사와 마법들이 가득한
+니가 내게 해준 이세계 여행', '2024-03-18', 1),
+('꿈결에서 (In dreams)', 'Single', '/songs/song04.jpg', 'https://youtu.be/sWbUbURskMQ', '아주 오래전에 꿈속에서 만 보았었던 이야기들은 멀어져가고 희미해지는 기억 속에서 너를 만나는 이야기들은
+
+이젠 멀어지는 너와의 기억 마저도
+나는요 알아요 우리가 소중했던
+많은 날들속에 지워진 사람들속에
+우리의 기억을 남겨두고 싶어요
+
+꿈에서만 볼 수 있어요 꿈에서만 만날 수 있어
+꿈속에서 너와 했었던 이야기들
+꿈은 아름답고 소중한 기억을 남겨
+너와의 추억도 지나간 감정들도
+
+사랑했는데 지나간 계절 속에서
+우리의 날들을 시간속에 새겼죠
+꿈을 그리워한 너와 나의 시간들은
+지나온 삶들에 마음을 담아내어
+
+꿈에서만 볼 수 있어요 꿈에서만 만날 수 있어
+꿈속에서 너와 했었던 이야기들
+꿈은 아름답고 소중한 기억을 남겨
+너와의 추억도 지나간 감정들도', '2024-09-23', 1),
+('Shine is mine', 'Single', '/songs/song05.jpg', 'https://youtu.be/UpMuBbh4O90', 'please let me go, (You) Always tell me
+Shine is mine and life is beautiful
+Look your dreaming, Love is wonderful
+Shining for your life
+
+날 보내줘,
+넌 언제나 말했었지 빛남은 내 것이라고
+너의 꿈을 내게도 보여줄래
+빛을 따라가
+
+Life is boring, Look out the window
+Falling stars are going to the dawn
+Summer days are out of living
+but please don''t let no more
+
+날 꺼내줘,
+이 세상이 어둠으로 가득 차 있더라도
+아직은 없는 꿈을 볼지라도
+빛을 따라가
+
+Please let me out, Even if you
+tell me about the storm and cloudy night,
+But don''t care about it
+Cause I''m stable
+Is that shine is mine
+
+Life is boring, eyes on sad days,
+Dreams are fading, I''m in the rain
+Stars were falling but I don''t care about it
+And please don''t let no more
+
+please let me go, (You) Always tell me
+Shine is mine and life is beautiful
+Look your dreaming, Love is wonderful
+Shining for your life', '2025-08-15', 1);
